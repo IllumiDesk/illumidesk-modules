fbae2b93ba7d6223a8446eddcc03340f79347b66
Abhilash Nair <abhilashnair1994@gmail.com>
2022-02-08 19:15:04 -0500

    removed role arn variable

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -8,11 +8,6 @@ variable "imageAddress" {
   default = "602401143452"
 }
 
-variable "roleARN" {
-  type    = string
-  default = "arn:aws:iam::898368509401:role/AmazonEKS_EFS_CSI_DriverRole"
-}
-
 variable "eks_iam_openid_connect_provider_arn" {
   type    = string
   default = module.eks_cluster.eks_iam_openid_connect_provider_arn
