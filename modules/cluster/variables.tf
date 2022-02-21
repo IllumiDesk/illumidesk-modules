8fb4ccdd41b3f41b2f4f0594c0217cce5b037f5d
Abhilash Nair <abhilashnair1994@gmail.com>
2022-02-21 11:47:03 -0500

    update variables defaults

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -10,6 +10,5 @@ variable "account_id" {
 
 variable "eks_iam_openid_connect_provider_url" {
   type    = string
-  default = "https://oidc.eks.us-west-2.amazonaws.com/id/6FBEFFE17A2B780AA8FB6C0E86808DF5"
 }
 
